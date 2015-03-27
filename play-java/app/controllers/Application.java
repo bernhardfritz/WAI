d6abe362c1c258976e87a69e959208d911c4d1ee
Bernhard Fritz <bernhard.e.fritz@gmail.com>
2015-03-27 15:27:18 +0100

    OpenProj

--- play-java/app/controllers/Application.java
+++ play-java/app/controllers/Application.java
@@ -6,9 +6,11 @@ import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
-
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
 
+    public static Result maptest() {
+        return ok(maptest.render());
+    }
 }
