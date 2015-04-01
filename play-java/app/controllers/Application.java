0cca4789646bcd1ac59225b56c6b11169e40fd6e
Bernhard Fritz <bernhard.e.fritz@gmail.com>
2015-04-01 13:02:29 +0200

    upload test

--- play-java/app/controllers/Application.java
+++ play-java/app/controllers/Application.java
@@ -1,9 +1,15 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import play.mvc.Controller;
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
+import play.mvc.Result;
+import views.html.index;
+import views.html.location;
+import views.html.map;
+import views.html.test;
 
-import views.html.*;
+import java.io.File;
 
 public class Application extends Controller {
     public static Result index() {
@@ -17,5 +23,23 @@ public class Application extends Controller {
     public static Result location(double lat, double lng) {
         System.out.println("lat: " + lat + " " + "lng: " + lng);
         return ok(location.render(lat,lng));
+    }
+
+    public static Result test() {
+        return ok(test.render());
+    }
+
+    public static Result upload() {
+        MultipartFormData body = request().body().asMultipartFormData();
+        FilePart picture = body.getFile("picture");
+        if (picture != null) {
+            String fileName = picture.getFilename();
+            String contentType = picture.getContentType();
+            File file = picture.getFile();
+            return ok("File uploaded");
+        } else {
+            flash("error", "Missing file");
+            return index();
+        }
     }
 }
